110a1b6618d91bdc3aaf492b1658bea298eea166
Udit Deo <uditdeo1670@gmail.com>
2019-10-26 17:22:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__, template_folder='template')
+model = pickle.load(open('house_model.pkl', 'rb'))
+
+@app.route('/')
+def index():
+    return render_template('ind1.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('ind1.html', prediction_text='House Price will be $ {}'.format(output))
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
